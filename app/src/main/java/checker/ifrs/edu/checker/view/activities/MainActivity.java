33821e54dac1a25fdd3ea7675aa7339e9242d91d
Fernando Sebenello Soares <sebenello@gmail.com>
2016-11-04 08:44:15 -0200

    Removido id duplicado

--- app/src/main/java/checker/ifrs/edu/checker/view/activities/MainActivity.java
+++ app/src/main/java/checker/ifrs/edu/checker/view/activities/MainActivity.java
@@ -1,19 +1,13 @@
 package checker.ifrs.edu.checker.view.activities;
 
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import checker.ifrs.edu.checker.R;
 
