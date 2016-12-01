66ee498900e37e3829b284f387e62dd610ab8e2c
Fernando Sebenello Soares <sebenello@gmail.com>
2016-12-01 13:17:58 -0200

    Salva o estado do RadioButton no ListView.

--- app/src/main/java/checker/ifrs/edu/checker/vo/RealmInt.java
+++ app/src/main/java/checker/ifrs/edu/checker/vo/RealmInt.java
@@ -18,7 +18,28 @@ public class RealmInt extends RealmObject{
         this.valor = valor;
     }
 
+    public void setValor(String opcao){
+        opcaoStringToInt(opcao);
+    }
+
     public int getValor(){
         return this.valor;
+    }
+
+    private void opcaoStringToInt(String opcao){
+        switch (opcao){
+            case "Sim":
+                this.valor = 1;
+                break;
+            case "Não":
+                this.valor = 2;
+                break;
+            case "Parcialmente":
+                this.valor = 3;
+                break;
+            case "Não se aplica":
+                this.valor = 4;
+                break;
+        }
     }
 }
--- app/src/main/java/checker/ifrs/edu/checker/vo/Resposta.java
+++ app/src/main/java/checker/ifrs/edu/checker/vo/Resposta.java
@@ -1,7 +1,10 @@
 package checker.ifrs.edu.checker.vo;
 
+import checker.ifrs.edu.checker.utils.ModelUtils;
+import io.realm.Realm;
 import io.realm.RealmList;
 import io.realm.RealmObject;
+import io.realm.RealmResults;
 import io.realm.annotations.PrimaryKey;
 
 public class Resposta extends RealmObject {
@@ -12,5 +15,79 @@ public class Resposta extends RealmObject {
     private RealmList<Questao> questoes;
     private RealmList<RealmInt> ints;
 
+    public Resposta(){
+        this.setId();
+    }
 
+    public Resposta(Avaliacao avaliacao, RealmList<Questao> questoes, RealmList<RealmInt> ints) {
+        this.setId();
+        this.avaliacao = avaliacao;
+        this.questoes = questoes;
+        this.ints = ints;
+    }
+
+    public Resposta(Avaliacao avaliacao, Questao questao, RealmInt inteiro) {
+        this.setId();
+        this.avaliacao = avaliacao;
+
+        this.questoes = new RealmList<>();
+        this.questoes.add(questao);
+
+        this.ints = new RealmList<>();
+        this.ints.add(inteiro);
+    }
+
+    public void setId() {
+        ModelUtils mModelUtils = new ModelUtils();
+        this.id = mModelUtils.getProximaIndex(Resposta.class);
+    }
+
+    public void setAvaliacao(Avaliacao avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public void setQuestoes(Questao questao) {
+
+        Realm realm = Realm.getDefaultInstance();
+        realm.beginTransaction();
+
+        this.removeIntsQuestao(this.questoes.indexOf(questao));
+
+        this.questoes.add(questao);
+
+        realm.commitTransaction();
+    }
+
+    public void setInts(RealmInt inteiro) {
+        Realm realm = Realm.getDefaultInstance();
+        realm.beginTransaction();
+        this.ints.add(inteiro);
+        realm.commitTransaction();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Avaliacao getAvaliacao() {
+        return avaliacao;
+    }
+
+    public RealmList<Questao> getQuestoes() {
+        return questoes;
+    }
+
+    public RealmList<RealmInt> getInts() {
+        return ints;
+    }
+
+    public void removeIntsQuestao(int position){
+        try{
+            this.questoes.remove(position);
+            this.ints.remove(position);
+        }catch (IndexOutOfBoundsException e){
+            e.getCause();
+            e.getMessage();
+        }
+    }
 }
