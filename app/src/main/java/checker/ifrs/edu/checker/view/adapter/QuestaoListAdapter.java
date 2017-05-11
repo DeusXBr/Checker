943d78581b0c5390cd2f5d4e65de2e6cc02f20ea
Fernando Sebenello Soares <sebenello@gmail.com>
2017-05-11 11:35:15 -0300

    Atualizado gradle.

--- app/src/main/java/checker/ifrs/edu/checker/view/adapter/QuestaoListAdapter.java
+++ app/src/main/java/checker/ifrs/edu/checker/view/adapter/QuestaoListAdapter.java
@@ -6,7 +6,6 @@ import android.util.SparseIntArray;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListAdapter;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
